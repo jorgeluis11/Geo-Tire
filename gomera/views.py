d4a80e68c439daad228a4438763cbb77686dd853
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-04-28 19:46:26 -0400

    Fix a few more PEP8 errors

--- gomera/views.py
+++ gomera/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import HttpResponse
 from django.views.generic import TemplateView
-import json, httplib
+import json
+import httplib
 
 
 class indexView(TemplateView):
-    template_name  = 'index.html'
+    template_name = 'index.html'
 
 
 def getCoordinates(request):
     if request.GET:
         connection = httplib.HTTPConnection('intrinsecus.crypticocorp.com', 80)
         connection.connect()
-        latitude  = request.GET['latitude']
+        latitude = request.GET['latitude']
         longitude = request.GET['longitude']
         connection.request('GET', 'http://intrinsecus.crypticocorp.com/api/namespaces/ads/sources/gomeras/data/?geometry__intersects=Point(['+longitude+','+latitude+']).buffer(0.05)&_format=json', '', {})
         result = json.loads(connection.getresponse().read())
