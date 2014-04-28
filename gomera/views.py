10d2bcf52c5b0115279ad084f69b729e27a880b4
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-04-28 19:26:20 -0400

    Usage of django.utils.simplejson is deprecated; replacing with json instead.

--- gomera/views.py
+++ gomera/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, HttpResponse
 from django.views.generic import TemplateView
 # Create your views here.
 import json,httplib,urllib
-from django.utils import simplejson
 
 class indexView(TemplateView):
 	template_name  = 'index.html'
@@ -15,6 +14,6 @@ def getCoordinates(request):
         longitude = request.GET['longitude']
         connection.request('GET', 'http://intrinsecus.crypticocorp.com/api/namespaces/ads/sources/gomeras/data/?geometry__intersects=Point(['+longitude+','+latitude+']).buffer(0.05)&_format=json', '', {})
         result = json.loads(connection.getresponse().read())
-        return HttpResponse(simplejson.dumps(result), content_type='application/json')
+        return HttpResponse(json.dumps(result), content_type='application/json')
     return HttpResponse('')
     
